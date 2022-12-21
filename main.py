f44f3ebd1d0608136ada43fb8b9494ca9058de6e
Mehank <70057473+M3hank@users.noreply.github.com>
2022-12-20 23:45:55 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,7 @@
-import requests
 import argparse
-import socket
-import concurrent.futures
-
+from core.dir_brute import dir_brute
+from core.sub_brute import sub_brute
+from core.port_scan import port_scan
 
 
 print("\033[0;36m   ____           __                   ___      ")
@@ -12,69 +11,27 @@ print("\033[0;36m/___//_\_\/ .__/_/\___/_/  \__/_/   /_/   \_, / ")
 print("\033[0;36m         /_/                             /___/  ")
 print("\033[0;36m                                            Github:- m3hank")
 
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('-d',help='Domain Name To Scan',dest='domain',required='true')
+    parser.add_argument('-t',help='Number of threads to Use',dest='threadcount',default='10',type=int)
+    parser.add_argument('-w',help=' Wordlist to Use',dest='wordlist')
+    parser.add_argument('-o',help='Store Output in a File',dest='output',action='store_true')
+    parser.add_argument('-dir',help='Directory brute-forcer',dest='direnum',action='store_true')
+    parser.add_argument('-se',help='Subdomain Enumeration',dest='subenum',action='store_true')
+    parser.add_argument('-ps',help='port-Scanning ',dest='portscan',action='store_true')
+    parser.add_argument('-p', help='Scan All The Ports Till User Input', dest='ports', type=int)
+    args = parser.parse_args()
 
-parser = argparse.ArgumentParser()
-parser.add_argument('-d',help='Domain Name To Scan',dest='domain',required='true')
-parser.add_argument('-t',help='Number of threads to Use',dest='thread',default='10',type=int)
-parser.add_argument('-dir',help='Directory brute-forcer',dest='dir',action='store_true')
-parser.add_argument('-ps',help='Scan Ports(Upto 1024)',dest='portscan',action='store_true')
-parser.add_argument('-se',help='Subdomain Enumeration',dest='subenum',action='store_true')
-parser.add_argument('-w',help=' Wordlist to Use',dest='wordlist')
-parser.add_argument('-o',help='Store Output in a File',dest='output',action='store_true')
-args = parser.parse_args()
-
-
-
-domain = args.domain
-threads = args.thread
-wordlist = args.wordlist
-
-
-with concurrent.futures.ThreadPoolExecutor(max_workers=threads) as executor:
-
-
-
- def brute_force(domain, wordlist):
-    with open(wordlist, 'r') as f:
-        for line in f:
-            url = f'http://{domain}/{line.strip()}'
-            r = requests.head(url)
-            if r.status_code == 200:
-                print(f'\033[32m Directory Found : {url} {r.status_code}')
-            else:
-                 print(f'\033[31m Directory Not Found : {url} {r.status_code}')
+    domain = args.domain
+    threadcount = args.threadcount
+    wordlist = args.wordlist
+    direnum = args.direnum
+    subenum = args.subenum
+    portscan = args.portscan
+    ports = args.ports
 
 
 
-def subdomain(subdomain):
-    try:
-        url = f"http://{subdomain}.{domain}"
-        r = requests.head(url)
-        if r.status_code == 200 or 301:
-            print(f"\033[0;32m Subdomain Found --> {subdomain}.{domain} {r.status_code}")
-        else: 
-         print(f"\033[0;31m Subdomain Found --> {subdomain}.{domain} {r.status_code}")
-    except:
-        print(f"\033[0;31m {subdomain}.{domain} ")
-
-
-
-if args.output == True:
-    with open("output.txt", "a") as f:
-                f.write(f"{subdomain}.{domain} \n")
-
-
-try:
-    if args.dir == True:
-        brute_force(domain, wordlist)
-except:
-    print("Please Provide a Wordlist!")
-
-
-try:
-    if args.subenum == True:
-        with open(wordlist, "r") as f:
-         for line in f:
-            subdomain(line.strip())
-except:
-    print("Please Provide a Wordlist!")
+    if __name__ == '__main__':
+     main()
